d6fa0a7a9b44775c1011ee7a8f49583f1ac9cbc1
Yuan Feng <yuanfeng.energy@gmail.com>
2017-04-19 14:44:13 -0700

    small improve

--- eduction_example/run_each.sh
+++ eduction_example/run_each.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 
 current_dir=${PWD}
 deepest_dir_array=( $(find . -type d -links 2 ) )
@@ -7,7 +7,16 @@ deepest_dir_array=( $(find . -type d -links 2 ) )
 for element in $(seq 0 $((${#deepest_dir_array[@]} - 1)))
 do
 	cd ${current_dir}/"${deepest_dir_array[$element]}"
-
-	essi -f main.fei
+	
+	if [ -f $PWD/require_parallel.hold ] # check if require essi_parallel
+		then
+			if hash essi_parallel 2>/dev/null; then # check if essi_parallel is available
+			    mpirun -np 4 essi_parallel -f main.fei
+			else
+			    echo "Example 27NodeBrick_DRM_3D require essi_parallel but it's not installed. Skip this example." 
+			fi
+	else
+		essi -f main.fei
+	fi
 
 done
