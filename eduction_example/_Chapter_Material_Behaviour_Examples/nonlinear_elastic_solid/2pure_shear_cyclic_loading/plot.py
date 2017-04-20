1c2fa90c60c7913c7653427091304340c483e624
Yuan Feng <yuanfeng.energy@gmail.com>
2017-04-20 16:21:24 -0700

    improved one example. Nonlinear elastic.

--- eduction_example/_Chapter_Material_Behaviour_Examples/nonlinear_elastic_solid/2pure_shear_cyclic_loading/plot.py
+++ eduction_example/_Chapter_Material_Behaviour_Examples/nonlinear_elastic_solid/2pure_shear_cyclic_loading/plot.py
@@ -1,14 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt  
 import h5py 
-h5in_filename = "elastic_shearing.h5.feioutput"
-h5in=h5py.File(h5in_filename,"r")
-outputs_all=h5in['/Model/Elements/Gauss_Outputs'][()]
-stress = outputs_all[16 , :-1]
-strain = outputs_all[4  , :-1]
+
+def h52stressStrain(h5in_filename):
+	h5in=h5py.File(h5in_filename,"r")
+	outputs_all=h5in['/Model/Elements/Gauss_Outputs'][()]
+	stress = outputs_all[14 , 1:-1]
+	strain = outputs_all[2  , 1:-1]
+	return [stress, strain]
+
+# [stress, strain]   = h52stressStrain("DP_1Confine.h5.feioutput")
+[stress_load,   strain_load]   = h52stressStrain("DP_1Confine.h5.feioutput")
+[stress_unload, strain_unload] = h52stressStrain("DP_2Release.h5.feioutput")
+stress  = np.concatenate((stress_load,stress_unload))
+strain  = np.concatenate((strain_load,strain_unload))
+
+# [stress_reload, strain_reload] = h52stressStrain("DP_3ReConfine.h5.feioutput")
+# stress  = np.concatenate((stress_load,stress_unload,stress_reload))
+# strain  = np.concatenate((strain_load,strain_unload,strain_reload))
+
+# plt.plot(strain, stress)
+# plt.show()
+
 plt.plot(strain, stress)
-plt.xlabel('strain')
-plt.ylabel('stress/(Pa)')
+plt.xlabel('Strain / (unitless)')
+plt.ylabel('Stress / (Pa)')
 plt.title('Material Behavior: Stress-Strain')
 plt.grid()
 plt.box()
