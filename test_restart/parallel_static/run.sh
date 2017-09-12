8428936ded171de9a6de53c9c34d0d9b3fe27857
Yuan <fengyuan.energy@gmail.com>
2017-09-11 21:01:31 -0700

    added parallel restart test case.

--- test_restart/parallel_static/run.sh
+++ test_restart/parallel_static/run.sh
@@ -1,5 +1,6 @@
-essi -f 0main_all.fei
-essi -f 1main_stage1.fei
-essi -f 2main_restart.fei
+mpirun -np 4 essi_parallel -f 0main_all.fei
+mpirun -np 4 essi_parallel -f 1main_stage1.fei
+mpirun -np 4 essi_parallel -f 2main_restart.fei
+
 h5diff t_2.h5.feioutput t_2_restart.h5.feioutput
 
