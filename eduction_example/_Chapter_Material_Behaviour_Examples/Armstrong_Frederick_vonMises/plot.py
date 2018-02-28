e25a41e6f066b2c824197bf52165373e0bce6942
Yuan <fengyuan.energy@gmail.com>
2018-02-27 19:54:44 -0800

    updated format.

--- eduction_example/_Chapter_Material_Behaviour_Examples/Armstrong_Frederick_vonMises/plot.py
+++ eduction_example/_Chapter_Material_Behaviour_Examples/Armstrong_Frederick_vonMises/plot.py
@@ -1,14 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt  
 
+# ===========================================
+# Font size
+# ===========================================
+import matplotlib
+font = {'family' : 'normal',
+        'weight' : 'bold',
+        'size'   : 22}
+matplotlib.rc('font', **font)
+# ===========================================
 
 strain = np.loadtxt("strain.feioutput")
 stress = np.loadtxt("stress.feioutput")
 
+stress_p = [ val/1000 for val in stress]
+strain_p = [ val*100 for val in strain]
 
-plt.plot(strain, stress)
-plt.xlabel('Strain / (unitless)')
-plt.ylabel('Stress / (Pa)')
+plt.figure(figsize=(10,8))
+plt.plot(strain_p, stress_p,'k', linewidth=2)
+plt.xlabel('Strain [%]')
+plt.ylabel('Stress [kPa]')
 plt.title('Material Behavior: Stress-Strain')
 plt.grid()
 plt.box()
